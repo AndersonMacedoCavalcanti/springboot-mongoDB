d7306a07999f93535afa280f19642be333467d25
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-01-30 14:16:53 -0300

    Inserção de usuário com POST

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.DTO.UserDTO;
 import com.example.demo.domain.entities.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.exception.ObjectNotFooundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,4 +25,18 @@ public class UserService {
         Optional<User> obj = userRepository.findById(id);
         return  obj.orElseThrow(() -> new ObjectNotFooundException("Objeto nao encontrado"));
     }
+
+    public User insert(User user){
+        return userRepository.save(user);
+    }
+
+    //fere um pouco o principio de reponsabilidade,
+    //mas colocamos para que possamos futuramente acessar,
+    //o banco de dados com a instancia userRepository
+
+    public  User fromDTO(UserDTO userDTO){
+        return  new User(userDTO.getId(),userDTO.getName(),userDTO.getEmail());
+    }
+
+
 }
