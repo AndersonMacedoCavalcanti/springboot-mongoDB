1b04834516d6fe0002ea46e2d2567ccca7c16501
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-01-30 15:36:51 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.DTO.AuthorDTO;
 import com.example.demo.domain.entities.Post;
 import com.example.demo.domain.entities.User;
 import com.example.demo.repository.PostRepository;
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
 
-        Post post1 = new Post(null, sdf.parse("21/01/2021"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!",maria);
-        Post post2 = new Post(null,sdf.parse("23/01/2021"),"Bom dia","Acordei feliz hoje!",maria);
+        Post post1 = new Post(null, sdf.parse("21/01/2021"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+        Post post2 = new Post(null,sdf.parse("23/01/2021"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
