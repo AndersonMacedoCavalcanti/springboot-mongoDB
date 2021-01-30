3c48507a6e9a54310f1a6ed3b30dd109b5797513
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-01-30 00:39:41 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/example/demo/domain/entities/User.java
+++ src/main/java/com/example/demo/domain/entities/User.java
@@ -1,11 +1,18 @@
 package com.example.demo.domain.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.processing.Generated;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document
 public class User implements Serializable {
 
 
+    @Id
     private String id;
     private String name;
     private String email;
