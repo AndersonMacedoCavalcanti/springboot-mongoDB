b3cbaa9307cdc3d722e8459330793b6e9b6031c5
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-01-30 20:07:14 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.entities.Post;
-import com.example.demo.domain.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    //Pode usar varios comando so usando o find..... ele gera a querie automaticamente
+    //IgnoreCase para ignorar letras maiusculas e minusculas
+    List<Post> findByTitleContainsIgnoreCase(String text);
 }
--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.entities.Post;
+import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,6 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
